ff87e1a4a0d9c6fd339d50fce76e463473f05a8c
Fabien <fabienwald@gmail.com>
2018-02-03 17:54:53 +0000

    Add Card.__repr__ for convenience

--- blackjack/card.py
+++ blackjack/card.py
@@ -9,3 +9,17 @@ class Card:
     def __init__(self, value, suit):
         self.value = value
         self.suit = suit
+
+    def __repr__(self):
+        if self.value == 1:
+            value = 'ace'
+        elif self.value == 11:
+            value = 'jack'
+        elif self.value == 12:
+            value = 'queen'
+        elif self.value == 13:
+            value = 'king'
+        else:
+            value = self.value
+
+        return f'{value} of {self.suit}'
